c5dfc3f8249bebce92e41000b38b1e9cf5a96f68
Trevor <trevorsemail96@gmail.com>
2016-07-21 10:29:24 -0500

    fixed issue with private channel

--- bot.js
+++ bot.js
@@ -77,8 +77,8 @@ listener.hears('!info',['direct_message','direct_mention','mention'],function(bo
 listener.hears('!help (.*)',['direct_message','direct_mention','mention'],function(bot,message){
 	bot.reply(message, 'You have requested help, the mentors have been notified and you will be contacted shortly.');
 	bot.api.users.info({user: message.user}, function(err, info){
-		bot.api.chat.postMessage({'token': slackToken, 'channel': 'mentor', 'text': 'User: ' + info.user.name+ ' has a question:\n' + message.text + ''});
-		bot.api.chat.postMessage({'token': slackToken, 'channel': 'privatetest', 'text': 'User: ' + info.user.name+ ' has a question:\n' + message.text + ''});
+	bot.api.chat.postMessage({'token': slackToken, 'channel': 'mentorchannel', 'text': 'User: ' + info.user.name+ ' has a question:\n' + message.text + '', 'username': 'SoHacks Bot', 'icon_emoji': ':floppy_disk:'});		
+b	ot.api.chat.postMessage({'token': slackToken, 'channel': 'privatetest', 'text': 'User: ' + info.user.name+ ' has a question:\n' + message.text + ''});
 	});
 	
 });
@@ -106,7 +106,7 @@ listener.hears('!countdown',['direct_message','direct_mention','mention'],functi
 listener.hears('!submit',['direct_message','direct_mention','mention'],function(bot,message){
 	bot.reply(message, 'Your question has been sent to the mentors');
 	bot.api.users.info({user: message.user}, function(err, info){
-		bot.api.chat.postMessage({'token': slackToken, 'channel': 'mentor', 'text': 'User: ' + info.user.name+ ' has a question:\n' + message.text + ''});
+		bot.api.chat.postMessage({'token': slackToken, 'channel': 'mentorchannel', 'text': 'User: ' + info.user.name+ ' has a question:\n' + message.text + '', 'username': 'SoHacks Bot', 'icon_emoji': ':floppy_disk:'});
 		bot.api.chat.postMessage({'token': slackToken, 'channel': 'privatetest', 'text': 'User: ' + info.user.name+ ' has a question:\n' + message.text + ''});
 
 	});
